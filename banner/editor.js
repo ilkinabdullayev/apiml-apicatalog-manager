e6ba54bb35005f460218711d475d2f450595f385
at670475 <andrea.tabone@broadcom.com>
2019-09-03 19:17:23 +0200

    Add Search and Save button

--- banner/editor.js
+++ banner/editor.js
@@ -1,10 +1,14 @@
+let searchButton = document.getElementById("searchButton");
+let searchBarHidden = true;
+let searchLabel = document.getElementsByClassName("ace_search right");
 function configureEditor() {
     var editor = ace.edit("editor");
-    // editor.setTheme("ace/theme/tomorrow_night_eighties");
+    editor.setTheme("ace/theme/tomorrow_night_eighties");
     editor.session.setMode("ace/mode/yaml");
     // editor.session.setValue("text")
     // editor.setReadOnly(false)
-    editor.execCommand("find")
+    // editor.execCommand("find");
+    // editor.searchBox.hide();
     editor.setOptions({
         autoScrollEditorIntoView: true,
         copyWithEmptySelection: true,
@@ -14,6 +18,20 @@ function configureEditor() {
         // wrap: true,
     });
 
+}
+
+searchButton.onclick = function() {
+
+    var editor = ace.edit("editor");
+    editor.execCommand("find");
+    if (searchBarHidden) {
+        editor.searchBox.show();
+        // editor.execCommand("find");
+        searchBarHidden = false;
+    } else if (!searchBarHidden) {
+        // editor.searchBox.hide();
+        searchBarHidden = true;
+    }
 }
 
 configureEditor();
