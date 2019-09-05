466ca384ace80f67878f3373ce0ba33b1ab3f7fc
at670475 <andrea.tabone@broadcom.com>
2019-09-05 13:06:06 +0200

    Fill textarea with real file from mainframe

--- banner/editor.js
+++ banner/editor.js
@@ -1,6 +1,5 @@
 let searchButton = document.getElementById("searchButton");
 let searchBarHidden = true;
-let fileItem = document.getElementById("item");
 
 function configureEditor() {
     var editor = ace.edit("editor");
@@ -21,7 +20,7 @@ function configureEditor() {
 
 }
 
-function getApiDefContent() {
+function getApiDefContentFileName() {
     // get('https://usilca32.lvn.broadcom.net:1443/zosmf/restfiles/fs?path=/z/masserv/mfaas/runtime/testb/zosmf.yml'
     getApiDef('https://usilca32.lvn.broadcom.net:1443/zosmf/restfiles/fs?path=/z/masserv/taban03/dev/instance/api-defs',
         (response) => {
@@ -35,19 +34,21 @@ function getApiDefContent() {
 function addItemsToLeftSidePanel(data) {
     let li = '';
     let count = 0;
+    let ul = document.getElementById("ussFiles");
     data.forEach((item) => {
         if (item.name.includes("yml") || item.name.includes(".properties")) {
-            li += '<li class="list-group-item" ><a id="fileItem' + count + '" class="nav-link" href="#item">' + item.name + '</li>'
+            li += '<li class="list-group-item" ><a id="fileItem' + count + '" class="fileItem" href="#item">' + item.name + '</li>'
+            console.log('fileItem'+count)
             count+=1;
         }
     });
 
-    let ul = document.getElementById("ussFiles");
     ul.innerHTML = li;
 
+    changeFile(ul);
 }
 
-getApiDefContent();
+getApiDefContentFileName();
 configureEditor();
 
 searchButton.onclick = function() {
@@ -64,7 +65,26 @@ searchButton.onclick = function() {
     }
 }
 
-fileItem.onclick = function(event) {
-    ace.edit("editor").setValue("");
+
+function changeFile(ul) {
+    ul.onclick = function(event) {
+        getFileContent(event.target.innerText);
+        event.preventDefault();
+    };
+}
+
+
+function getFileContent(clickedElement) {
+     getApiDefContentFile('https://usilca32.lvn.broadcom.net:1443/zosmf/restfiles/fs/z/masserv/taban03/dev/instance/api-defs/' + clickedElement,
+        (response) => {
+         console.log(response.responseText)
+         fillTextAreaWithFile(response.responseText.toString());
+        });
+}
+
+function fillTextAreaWithFile(data) {
+    ace.edit("editor").setValue(data);
     event.preventDefault();
 }
+
+
