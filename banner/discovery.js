b23974ef48925929a902a4adc4265062bfe3347c
at670475 <andrea.tabone@broadcom.com>
2019-09-06 10:53:41 +0200

    Change eureka content based on selection

--- banner/discovery.js
+++ banner/discovery.js
@@ -1,4 +1,6 @@
 //Eureka
+let select = document.getElementById('discoveryDropdown');
+
 function initializeEureka() {
     let serviceId = getParamValue('serviceId');
     serviceId = (serviceId == 'dashboard') ? '' : '/' + serviceId;
@@ -7,9 +9,45 @@ function initializeEureka() {
             const jsonResponse = JSON.parse(response.responseText);
 
             const jsonViewer = new JSONViewer();
-            document.getElementById('eureka-panel').appendChild(jsonViewer.getContainer());
+            document.getElementById('eurekaInstancesViewer').appendChild(jsonViewer.getContainer());
             jsonViewer.showJSON(jsonResponse);
+            fillDropdownBar(jsonResponse)
         });
 }
 
 initializeEureka();
+
+function fillDropdownBar(response) {
+    let applications = response.applications.application;
+    let servicesName = [];
+    applications.forEach((item) => {
+        servicesName.push(item.name)
+    });
+    addElementToDropdownBar(servicesName);
+}
+
+function addElementToDropdownBar(data) {
+    let dropdown = document.getElementById('discoveryDropdown');
+    data.forEach((item) => {
+        let opt = document.createElement('option');
+        opt.innerHTML = item;
+        dropdown.appendChild(opt);
+    })
+}
+
+select.onchange = function (event) {
+    let serviceId = this.value;
+    changeEureka(serviceId);
+}
+
+function changeEureka(serviceId) {
+    get('https://localhost:10011/eureka/apps/' + serviceId,
+        (response) => {
+            const jsonResponse = JSON.parse(response.responseText);
+            const jsonViewer = new JSONViewer();
+            document.getElementById('eurekaInstancesViewer').innerHTML = "";
+            document.getElementById('eurekaInstancesViewer').appendChild(jsonViewer.getContainer());
+            jsonViewer.showJSON(jsonResponse);
+            console.log(jsonResponse)
+        });
+}
